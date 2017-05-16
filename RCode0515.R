c6edf27566c440a103203e4af6723ccef2251b3d
zhangxr91 <363924002@qq.com>
2017-05-15 23:48:29 -0500

    Updated0515-2348

--- RCode0515.R
+++ RCode0515.R
@@ -2,7 +2,8 @@ install.packages('dplyr') # install package
 install.packages('ggthemes')
 install.packages('mice')
 install.packages('randomForest')
-# ===================
+---------------
+#Load and check data
 library('readr')
 library('ggplot2') # visualization
 library('ggthemes') # visualization
@@ -16,6 +17,8 @@ train <- read_csv("~/Desktop/Titanic/Data/train.csv") #import data
 test <- read_csv("~/Desktop/Titanic/Data/test.csv")
 full <- bind_rows(train,test) # combine dataset
 
-
 str(full) # display structure of data
+--------
+#Feature Engineering
+full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
 
